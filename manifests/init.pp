8b22b30b4b443d5fb3c114079ec2e91303ffd2fe
mh <mh@immerda.ch>
2008-07-04 15:22:38 +0000

    added rpm infos to find packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,8 @@
 # modules/daemontools/manifests/init.pp - manage daemontools stuff
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
+# For rpms for redhat, centos etc. you might want to look here:
+# http://summersoft.fay.ar.us/pub/qmail/daemontools/
 
 # modules_dir { "daemontools": }
 
