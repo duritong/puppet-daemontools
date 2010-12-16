8023b5375e6e56afa6222fa6fd5a3d25b4f68cb3
mh <mh@immerda.ch>
2010-12-16 12:55:34 +0100

    add missing dependency

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,7 +1,8 @@
 class daemontools::centos inherits daemontools::base {
-    # start svscan, it is added to the inittab by the rpm
-    # but not started
-    exec{'svscanboot &':
-        unless => 'ps ax | grep -v grep | grep -q svscan',
-    }
+  # start svscan, it is added to the inittab by the rpm
+  # but not started
+  exec{'svscanboot &':
+    unless => 'ps ax | grep -v grep | grep -q svscan',
+    require => Package['daemontools'],
+  }
 }
